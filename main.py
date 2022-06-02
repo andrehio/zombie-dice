41fbef705738db69a22e63c9ae25038c4f23653f
Andre Hioki <ahdreki@gmail.com>
2022-06-01 22:44:25 -0300

    1.3
    number of players, turn of each player, take a die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Zombie Dice Game
-Date: 05/may/2022
+Date: 09/may/2022
 Author: André Hioki
 
 Description: Development in python language of the game zombie dice.
@@ -14,6 +14,26 @@ dices = {
     "red_qty": 3, "red_faces": "SFSBFS"
     }
 
+# ----number of players----
+qty_players = "0"
+while int(qty_players) < 2:
+    qty_players = input("Enter number of players:")
+    try:
+        if int(qty_players) < 2:
+            print("Error: it must have at least 2 players")
+    except ValueError:
+        print("Error! Please enter a number")
+        qty_players = "0"
+
+qty_players_int = int(qty_players)
+
+# ----Name of players----
+name_players = []
+print("Enter the name of each player:")
+for counter1 in range(qty_players_int):
+    name_players.append(input("Player " + str(counter1 + 1) + ": "))
+
+# ----dice in the shaker cup----
 shaker_cup = ""
 for i in range(dices["green_qty"]):
     shaker_cup += "G"
@@ -29,7 +49,7 @@ if int(qty_players) < 2:
     print("Error: it must have at least 2 players")
     qty_players = input("Enter number of players:")
 
-# Shuffle dices inside the tube
+# Shuffle dices inside the shaker cup
 dice_sequence = random.choice(shaker_cup)
 
 faces = ""
@@ -47,4 +67,37 @@ else:
     faces += random.choice(dices["red_faces"])
     shotgun += 1
 
-print(faces)
+# ----Turn of each player----
+dice_qty = dices["green_qty"] + dices["yellow_qty"] + dices["red_qty"]
+for counter1 in range(qty_players_int):
+    brain = 0
+    shotgun = 0
+    round_option = 2
+    lock = 0
+    dice_locked = []
+    turn = 1
+
+    # ----Take a die----
+    chosen_dice = random.choice(shaker_cup)
+
+    print("\nPlayer " + name_players[counter1] + "'s turn")
+
+    # ********insert round_option
+
+    if chosen_dice == "G":
+        chosen_face = random.choice(dices["green_faces"])
+    elif chosen_dice == "Y":
+        chosen_face = random.choice(dices["yellow_faces"])
+    else:
+        chosen_face = random.choice(dices["red_faces"])
+
+    print("Chosen dices")
+    if chosen_face == "B":
+        print(chosen_dice + " dice: brain")
+        brain += 1
+    elif chosen_face == "F":
+        print(chosen_dice + " dice: footprint")
+    else:
+        print(chosen_dice + " dice: shotgun")
+        shotgun += 1
+        lock += 1
