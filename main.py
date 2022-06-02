1e588be2745bdfcd542f3bd082af30eec820afcf
Andre Hioki <ahdreki@gmail.com>
2022-06-02 11:01:10 -0300

    1.3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Zombie Dice Game
-Date: 09/may/2022
+Date: 16/may/2022
 Author: André Hioki
 
 Description: Development in python language of the game zombie dice.
@@ -13,11 +13,12 @@ dices = {
     "yellow_qty": 4, "yellow_faces": "SFBSFB",
     "red_qty": 3, "red_faces": "SFSBFS"
     }
+dice_qty = dices["green_qty"] + dices["yellow_qty"] + dices["red_qty"]
 
 # ----number of players----
 qty_players = "0"
 while int(qty_players) < 2:
-    qty_players = input("Enter number of players:")
+    qty_players = input("Enter number of players: ")
     try:
         if int(qty_players) < 2:
             print("Error: it must have at least 2 players")
@@ -29,46 +30,23 @@ qty_players_int = int(qty_players)
 
 # ----Name of players----
 name_players = []
-print("Enter the name of each player:")
+print("Enter the name of each player: ")
 for counter1 in range(qty_players_int):
     name_players.append(input("Player " + str(counter1 + 1) + ": "))
 
 # ----dice in the shaker cup----
-shaker_cup = ""
-for i in range(dices["green_qty"]):
-    shaker_cup += "G"
+shaker_cup = []
+for counter1 in range(dices["green_qty"]):
+    shaker_cup.append("Green")
 
-for i in range(dices["yellow_qty"]):
-    shaker_cup += "Y"
+for counter1 in range(dices["yellow_qty"]):
+    shaker_cup.append("Yellow")
 
-for i in range(dices["red_qty"]):
-    shaker_cup += "R"
-
-qty_players = input("Enter number of players:")
-if int(qty_players) < 2:
-    print("Error: it must have at least 2 players")
-    qty_players = input("Enter number of players:")
-
-# Shuffle dices inside the shaker cup
-dice_sequence = random.choice(shaker_cup)
-
-faces = ""
-brain = 0
-footprint = 0
-shotgun = 0
-
-if dice_sequence == "G":
-    faces += random.choice(dices["green_faces"])
-    brain += 1
-elif dice_sequence == "Y":
-    faces += random.choice(dices["yellow_faces"])
-    footprint += 1
-else:
-    faces += random.choice(dices["red_faces"])
-    shotgun += 1
+for counter1 in range(dices["red_qty"]):
+    shaker_cup.append("Red")
 
 # ----Turn of each player----
-dice_qty = dices["green_qty"] + dices["yellow_qty"] + dices["red_qty"]
+brain_players = []
 for counter1 in range(qty_players_int):
     brain = 0
     shotgun = 0
@@ -77,27 +55,63 @@ for counter1 in range(qty_players_int):
     dice_locked = []
     turn = 1
 
-    # ----Take a die----
-    chosen_dice = random.choice(shaker_cup)
+    print("\n------" + name_players[counter1] + "'s turn------")
+    while round_option == 2:
+        print("Turn " + str(turn))
+        print("Thrown dice")
+        for counter2 in range(3):
+            condition = 0
+            while condition == 0:
+                chosen_dice_random = random.randint(0, dice_qty - 1)
+                if lock != 0:
+                    for counter3 in range(lock):
+                        if chosen_dice_random != dice_locked[counter3]:
+                            condition = 1
+                else:
+                    condition = 1
 
-    print("\nPlayer " + name_players[counter1] + "'s turn")
+            # ----take a die----
+            chosen_dice = shaker_cup[chosen_dice_random]
 
-    # ********insert round_option
+            if chosen_dice == "Green":
+                chosen_face = random.choice(dices["green_faces"])
+            elif chosen_dice == "Yellow":
+                chosen_face = random.choice(dices["yellow_faces"])
+            else:
+                chosen_face = random.choice(dices["red_faces"])
 
-    if chosen_dice == "G":
-        chosen_face = random.choice(dices["green_faces"])
-    elif chosen_dice == "Y":
-        chosen_face = random.choice(dices["yellow_faces"])
-    else:
-        chosen_face = random.choice(dices["red_faces"])
+            # ----roll the die----
+            if chosen_face == "B":
+                print(str(counter2 + 1) + ") " + chosen_dice + " die: brain")
+                brain += 1
+            elif chosen_face == "F":
+                print(str(counter2 + 1) + ") " + chosen_dice + " die: footprint")
+            else:
+                print(str(counter2 + 1) + ") " + chosen_dice + " die: shotgun")
+                shotgun += 1
+                lock += 1
+                dice_locked.append(chosen_dice_random)
 
-    print("Chosen dices")
-    if chosen_face == "B":
-        print(chosen_dice + " dice: brain")
-        brain += 1
-    elif chosen_face == "F":
-        print(chosen_dice + " dice: footprint")
-    else:
-        print(chosen_dice + " dice: shotgun")
-        shotgun += 1
-        lock += 1
+        # ----check victory/failure condition and option to continue the turn----
+        if shotgun >= 3:
+            round_option = 1
+            brain = 1
+            print("------Player " + name_players[counter1] + " lost!------")
+        elif brain >= 13 and shotgun < 3:
+            round_option = 3
+            print("------Player " + name_players[counter1] + " won!------")
+        else:
+            round_option = 0
+            while not (round_option == 1 or round_option == 2):
+                try:
+                    print("\nCurrent numbers: Brain: ", str(brain), " / Shotgun: ", str(shotgun))
+                    print("Do you like to stay in your turn and roll new dice?")
+                    round_option = int(input("Type '1' to NO; or type '2' to YES: "))
+                except ValueError:
+                    print("\nError! Please enter a number")
+
+        turn += 1
+
+    #brain_players.append(brain)
+    if round_option == 3:
+        break
