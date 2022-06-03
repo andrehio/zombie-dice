87aa96cf2d03149dae39f8e0512f700df9b31cc0
Andre Hioki <ahdreki@gmail.com>
2022-06-03 09:58:15 -0300

    1.4

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """
 Zombie Dice Game
 Date: 16/may/2022
+Version: 1.4
 Author: André Hioki
 
 Description: Development in python language of the game zombie dice.
 """
 import random
+import time
+import operator
 
 # Dice's face: B-Brain, F-Footprint, S-Shotgun
 dices = {
@@ -32,7 +35,28 @@ qty_players_int = int(qty_players)
 name_players = []
 print("Enter the name of each player: ")
 for counter1 in range(qty_players_int):
-    name_players.append(input("Player " + str(counter1 + 1) + ": "))
+    name_players.append(input(f"Player {counter1 + 1}: "))
+
+    # ----players' order----
+print("Player order")
+player_order = "0"
+while int(player_order) == 0:
+    player_order = input("Type '1' to keep the players order; or type '2' to shuffle the players order: ")
+    try:
+        if int(player_order) > 2:
+            print("Error: Type 1 or 2")
+            player_order = "0"
+    except ValueError:
+        print("Error! Please enter a number")
+        player_order = "0"
+
+if player_order == "2":
+    random.shuffle(name_players)
+
+# ----Dict Brains----
+brain_players = {}
+for counter1 in range(len(name_players)):
+    brain_players[name_players[counter1]] = None
 
 # ----dice in the shaker cup----
 shaker_cup = []
@@ -46,7 +70,6 @@ for counter1 in range(dices["red_qty"]):
     shaker_cup.append("Red")
 
 # ----Turn of each player----
-brain_players = []
 for counter1 in range(qty_players_int):
     brain = 0
     shotgun = 0
@@ -55,9 +78,9 @@ for counter1 in range(qty_players_int):
     dice_locked = []
     turn = 1
 
-    print("\n------" + name_players[counter1] + "'s turn------")
+    print(f"\n------{name_players[counter1]}'s turn------")
     while round_option == 2:
-        print("Turn " + str(turn))
+        print(f"\nTurn {turn}")
         print("Thrown dice")
         for counter2 in range(3):
             condition = 0
@@ -82,29 +105,30 @@ for counter1 in range(qty_players_int):
 
             # ----roll the die----
             if chosen_face == "B":
-                print(str(counter2 + 1) + ") " + chosen_dice + " die: brain")
+                print(f"{counter2 + 1}) {chosen_dice} die: brain face")
                 brain += 1
             elif chosen_face == "F":
-                print(str(counter2 + 1) + ") " + chosen_dice + " die: footprint")
+                print(f"{counter2 + 1}) {chosen_dice} die: footprint face")
             else:
-                print(str(counter2 + 1) + ") " + chosen_dice + " die: shotgun")
+                print(f"{counter2 + 1}) {chosen_dice} die: shotgun face")
                 shotgun += 1
                 lock += 1
                 dice_locked.append(chosen_dice_random)
+            time.sleep(1)
 
         # ----check victory/failure condition and option to continue the turn----
         if shotgun >= 3:
             round_option = 1
             brain = 1
-            print("------Player " + name_players[counter1] + " lost!------")
+            print(f"------Player {name_players[counter1]} lost!------")
         elif brain >= 13 and shotgun < 3:
             round_option = 3
-            print("------Player " + name_players[counter1] + " won!------")
+            print(f"------Player {name_players[counter1]} won!------")
         else:
             round_option = 0
             while not (round_option == 1 or round_option == 2):
                 try:
-                    print("\nCurrent numbers: Brain: ", str(brain), " / Shotgun: ", str(shotgun))
+                    print(f"\nCurrent numbers: Brain: {brain} / Shotgun: {shotgun}")
                     print("Do you like to stay in your turn and roll new dice?")
                     round_option = int(input("Type '1' to NO; or type '2' to YES: "))
                 except ValueError:
@@ -112,6 +136,15 @@ for counter1 in range(qty_players_int):
 
         turn += 1
 
-    #brain_players.append(brain)
+    brain_players[name_players[counter1]] = brain
     if round_option == 3:
         break
+
+print("\n")
+print("*" * 30)
+print("**********Scoreboard**********")
+print("*" * 30)
+
+player_ranked = sorted(brain_players.items(), key=operator.itemgetter(1), reverse=True)
+for counter1, v in enumerate(player_ranked):
+    print(f"{counter1+1}º - {v[0]}: {v[1]}")
